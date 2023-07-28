a367b1b8045f4d0adf7726d137ab376d303fdd58
meet_goti <87909092+meet-31@users.noreply.github.com>
2023-07-28 17:30:18 +0530

    Add files via upload

---
+++ test/predict_winner.cpp
@@ -0,0 +1,17 @@
+// predict_winner.cpp
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        
+    }
+    return 0;
+}
